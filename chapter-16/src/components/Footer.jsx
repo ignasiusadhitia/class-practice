7514e249b5665fd5b45faaf27630b008eb6a34cb
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-10-28 17:14:16 +0700

    feat: add footer

--- chapter-16/src/components/Footer.jsx
+++ chapter-16/src/components/Footer.jsx
@@ -7,50 +7,52 @@ import NavList from "./navigation/NavList";
 const Footer = ({ items }) => {
   const { services, icons, careers } = items;
   return (
-    <footer>
-      <FooterItem title="About Us">
-        <p>
-          It is a long established fact that a reader will be distracted by the
-          readable content of a page when looking at
-        </p>
+    <footer className="footer-section">
+      <div className="container footer-item-wrapper">
+        <FooterItem title="About Us">
+          <p>
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at
+          </p>
 
-        <div>
-          {icons.map((icon, index) => (
-            <Icon iconClassName={icon.iconClassName} key={index} />
-          ))}
-        </div>
-      </FooterItem>
+          <div>
+            {icons.map((icon, index) => (
+              <Icon iconClassName={icon.iconClassName} key={index} />
+            ))}
+          </div>
+        </FooterItem>
 
-      <FooterItem title="Services">
-        <NavList items={services} />
-      </FooterItem>
+        <FooterItem title="Services">
+          <NavList items={services} />
+        </FooterItem>
 
-      <FooterItem title="Career">
-        <div>
-          {careers.map((career, index) => (
-            <div key={index}>
-              <Icon iconClassName={career.iconClassName} />
-              <div>
-                <h6>{career.title}</h6>
-                <span>{career.experience}</span>
+        <FooterItem title="Career">
+          <div>
+            {careers.map((career, index) => (
+              <div key={index}>
+                <Icon iconClassName={career.iconClassName} />
+                <div>
+                  <h6>{career.title}</h6>
+                  <span>{career.experience}</span>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-      </FooterItem>
+            ))}
+          </div>
+        </FooterItem>
 
-      <FooterItem title="Subscribe Us">
-        <p>
-          It is a long established fact that a reader will be distracted by the
-          readable
-        </p>
+        <FooterItem title="Subscribe Us">
+          <p>
+            It is a long established fact that a reader will be distracted by
+            the readable
+          </p>
 
-        <form>
-          <input type="email" name="" id="" />
+          <form>
+            <input type="email" name="" id="" />
 
-          <Button text="Submit" />
-        </form>
-      </FooterItem>
+            <Button text="Submit" />
+          </form>
+        </FooterItem>
+      </div>
     </footer>
   );
 };
